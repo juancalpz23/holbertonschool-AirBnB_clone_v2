2a1cca8702318cdd4d10ded43db049b49c07341c
Juan C Lopez <juancalpz@yahoo.com>
2024-04-11 11:10:38 -0400

    Delete - Eliminated files copied from fork

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,19 +1,10 @@
 #!/usr/bin/python3
-'''
-    Script that starts a Flask web application
-'''
+"""
+Script that starts a Flask web application
+"""
 from flask import Flask
 
 app = Flask(__name__)
 
 
-@app.route('/', strict_slashes=False)
-def hello():
-    '''
-        Returns a string to the root route / of the web application
-    '''
-    return 'Hello HBNB!'
-
-
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+app.route('/', )
