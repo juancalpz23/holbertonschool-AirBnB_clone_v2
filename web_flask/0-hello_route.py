c0613558b2ce59fc3f39815c5e2e04dc2b455121
Juan C Lopez <juancalpz@yahoo.com>
2024-04-11 12:34:48 -0400

    Update - typo in app.route

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -2,15 +2,16 @@
 """
 Script that starts a Flask web application
 """
+
 from flask import Flask
 
 app = Flask(__name__)
 
 
-app.route('/', strict_slashes=False)
+@app.route('/', strict_slashes=False)
 def hello_hbnb():
     """
-    Display 'Hello HBNB!'
+    Displays 'Hello HBNB!'
     """
     return 'Hello HBNB!'
 
