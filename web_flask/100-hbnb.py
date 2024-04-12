c286e839997ef436294b17f15d79d0202f80944f
Juan C Lopez <juancalpz@yahoo.com>
2024-04-12 17:59:00 -0400

    Add - new files for the extra task

---
+++ web_flask/100-hbnb.py
@@ -0,0 +1,36 @@
+#!/usr/bin/python3
+"""
+Starts a Flask web application
+"""
+
+from flask import Flask, render_template
+from models import storage, State, City, Amenity, Place
+
+app = Flask(__name__)
+app.url_map.strict_slashes = False
+
+
+@app.route('/hbnb')
+def hbnb():
+    """
+    Displays a HTML page like 8-index.html from the AirBnB clone project
+    """
+    states = storage.all(State).values()
+    cities = storage.all(City).values()
+    amenities = storage.all(Amenity).values()
+    places = storage.all(Place).values()
+
+    return render_template('100-hbnb.html', states=states, cities=cities,
+                           amenities=amenities, places=places)
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """
+    Remove the current SQLAlchemy Session after each request
+    """
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
