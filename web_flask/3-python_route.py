ed07ac098f1c8a91fb11b532395044da5435f870
Juan C Lopez <juancalpz@yahoo.com>
2024-04-12 11:05:40 -0400

    Update - Changed to {}.format and pycodestyle

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -15,6 +15,7 @@ def hello_hbnb():
     """
     return 'Hello HBNB!'
 
+
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
     """
@@ -22,17 +23,19 @@ def hbnb():
     """
     return 'HBNB'
 
+
 @app.route('/c/<text>', strict_slashes=False)
 def c_text(text):
     """
     Displays C followed by the of text variable
     """
-    return 'C' + text.replace('_', ' ')
+    return 'C {}'.format(text.replace('_', ' '))
+
 
 @app.route('/python/', defaults={'text': 'is_cool'}, strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def py_text(text):
-    return 'Python' + text.replace('_', ' ')
+    return 'Python {}'.format(text.replace('_', ' '))
 
 
 if __name__ == '__main__':
