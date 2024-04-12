2fc90eca29507359ec20eb18d61c59452bd4f54c
Juan C Lopez <juancalpz@yahoo.com>
2024-04-12 15:28:06 -0400

    Add - file 8-cities_by_states.py - testing new format

---
+++ web_flask/8-cities_by_states.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+"""
+Starts a Flask web application
+"""
+
+from flask import Flask, render_template
+from models import storage, classes
+from models.state import State
+
+app = Flask(__name__)
+
+
+@app.route('/cities_by_states', strict_slashes=False)
+def cities_by_state():
+    """
+    Displays html with states and cities listed
+    """
+    states = storage.all(classes["State"]).values()
+    states_sorted = sorted(states, key=lambda state: state.name)
+    return (render_template('8-states_list.html', states=states_sorted))
+
+
+@app.teardown_appcontext
+def teardown_db(exception):
+    """
+    Close storage session
+    """
+    storage.close()
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
