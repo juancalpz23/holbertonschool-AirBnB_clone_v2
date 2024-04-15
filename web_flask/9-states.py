3c8240638c32101fddb64b50f00fa0d8fa60db51
Juan C Lopez <juancalpz@yahoo.com>
2024-04-15 14:44:48 -0400

    Update - Fixed /state/ app.route and html format

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -3,6 +3,8 @@
 Starts a Flask web application
 """
 
+import sys
+sys.path.append("c:\\Users\\juanc\\OneDrive\\Documents\\Visual Studio\\Holberton\\holbertonschool-AirBnB_clone_v2")
 from flask import Flask, render_template
 from models import storage, classes
 from models.state import State
@@ -11,14 +13,71 @@ from models.city import City
 app = Flask(__name__)
 
 
-@app.route('/states', strict_slashes=False)
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """
+    Displays 'Hello HBNB!'
+    """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def show_hbnb():
+    """
+    Displays 'HBNB'
+    """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """
+    Displays C followed by the of text variable
+    """
+    return 'C {}'.format(text.replace('_', ' '))
+
+
+@app.route('/python/', defaults={'text': 'is_cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def py_text(text):
+    """
+    Display 'Python' followed by text
+    """
+    return 'Python {}'.format(text.replace('_', ' '))
+
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def display_num(n):
+    """
+    Displays 'n' number only if integer
+    """
+    return '{} is a number'.format(n)
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def display_num_template(n):
+    """
+    Display an HTML page if n is an integer
+    """
+    return render_template('5-number.html', n=n)
+
+
+@app.route('/number_odd_or_even/<int:n>', strict_slashes=False)
+def display_odd_even(n):
+    """
+    Display a HTML page only if n is an integer, and if it is odd or eve
+    """
+    return render_template('6-number_odd_or_even.html', n=n)
+
+
+@app.route('/states_list', strict_slashes=False)
 def states_list():
     """
-    Displays a list of states
+    Displays html with states listed
     """
     states = storage.all(classes["State"]).values()
     states_sorted = sorted(states, key=lambda state: state.name)
-    return render_template('7-states_list.html', states=states_sorted)
+    return (render_template('7-states_list.html', states=states_sorted))
 
 
 @app.route('/cities_by_states', strict_slashes=False)
@@ -31,17 +90,20 @@ def cities_by_state():
     return (render_template('8-cities_by_states.html', states=states_sorted))
 
 
+@app.route('/states', strict_slashes=False)
 @app.route('/states/<id>', strict_slashes=False)
-def state_cities(id):
+def state_cities(id=None):
     """
-    Displays cities of a specific state
+    Displays cities of a specific state or a state
     """
-    state = storage.get(classes["State"], id)
-    if state:
-        cities = sorted(state.cities, key=lambda city: city.name)
-        return render_template('9-states.html', state=state, cities=cities)
+    states = storage.all(classes["State"]).values()
+    if id:
+        for state in states:
+            if state.id == id:
+                return render_template('9-states.html', state=state)
+        return render_template('9-states.html')
     else:
-        return render_template('not_found.html')
+        return render_template('9-states.html', states=states)
 
 
 @app.teardown_appcontext
